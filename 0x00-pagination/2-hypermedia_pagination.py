6b6a0738c6341b57aafab200995303ad30f78c1c
Bayero-abdul <bayeroa65@gmail.com>
2023-10-23 21:05:09 +0100

    Test

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,8 +51,8 @@ class Server:
         data = self.get_page(page, page_size)
         dataset_len = len(self.dataset())
         total_pages = math.ceil(dataset_len / page_size)
-        next_page = page + 1 if data and page <= total_pages else None
-        prev_page = page - 1 if data and page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
         page_size = len(data)
         return {
             'page_size': page_size,
