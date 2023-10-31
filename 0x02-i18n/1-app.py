ebd9080bc364706e4547399ef8bc9c5af805e101
Bayero-abdul <bayeroa65@gmail.com>
2023-10-31 20:40:01 +0100

    1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""1-app.py """
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """App Configuration. """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello():
+    """Greets. """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
